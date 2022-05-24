ac95c7b3d1affbb75cba14299e8ff480356450e5
pedrocore22 <pedrocore22@itweblearning.com>
2022-05-24 11:30:46 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { P404Component } from './p404/p404.component';
+import { SoporteComponent } from './soporte/soporte.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: InicioComponent},
+  {path:'soporte', component: SoporteComponent},
+  {path: '**', component: P404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SoporteComponent } from './soporte/soporte.component';
+import { HeaderComponent } from './header/header.component';
+import { P404Component } from './p404/p404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SoporteComponent,
+    HeaderComponent,
+    P404Component
   ],
   imports: [
     BrowserModule,
